5be17b8a9d5c4324571992bf90f032898d8773c5
Feng Yuxiang <2017feng35@gmail.com>
2025-12-03 12:51:11 +0800

    add mo3, dnf control

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let results = [];
 let sorted_result = [];
 let locked = false;
 let goal = 10;
+let mode = "ao5"; // or "mo3"
+let current_result = -1
 
 let timer_block = document.getElementById("timer");
 let scramble_block = document.getElementById("scramble");
@@ -32,11 +34,11 @@ document.addEventListener("keydown", (e) => {
         stopTimer();
     }
     else {
+        if (locked) {
+            return;
+        }
         if (e.code === "Space") {
-            if (locked) {
-                return;
-            }
-            e.preventDefault();  // 阻止页面滚动
+            e.preventDefault();  // Prevent rolling
             if (!running) {
                 timer_block.style.color = c_ready;
                 isReady = true;
@@ -45,6 +47,9 @@ document.addEventListener("keydown", (e) => {
         }
         if (e.code == "KeyR") {
             reset();
+        }
+        if (e.code == "KeyD") {
+            switchDNF();
         }
         if (e.code == "ArrowRight") {
             nextScramble();
@@ -82,8 +87,24 @@ document.addEventListener("keydown", (e) => {
                 goal = Number(input);
                 changeStorage("goal", goal);
             }
-
         }
+
+        if (e.code == "KeyM") {
+            notice_block.textContent = "Select Mode: 5 - ao5, 3 - mo3";
+        }
+
+        else if (lastKey == "KeyM") {
+            if (e.code == "Digit5") {
+                changeMode("ao5");
+                notice_block.textContent = "";
+            } else if (e.code == "Digit3") {
+                changeMode("mo3");
+                notice_block.textContent = "";
+            } else {
+                notice_block.textContent = "";
+            }
+        }
+
         lastKey = e.code;
     }
 });
@@ -106,11 +127,12 @@ function stopTimer() {
     showElements();
     scramble_block.textContent = genScramble();
     r = Number(timer_block.textContent);
+    current_result = r;
     addResult(r);
     locked = true;
     setTimeout(() => {
         locked = false;
-    }, 1000);
+    }, 500);
 }
 
 function startTimer() {
@@ -135,29 +157,44 @@ function showElements() {
 }
 
 function addResult(t) {
-    if (results.length < 5) {
+    let n;
+    if (mode == "ao5")
+        n = 5;
+    else if (mode == "mo3")
+        n = 3;
+
+    if (results.length < n) {
         results.push(t);
-        sorted_result = results.slice().sort((a, b) => a - b);
+        // sorted_result = results.slice().sort((a, b) => a - b);
+        sorted_result = mysort(results);
         document.getElementById("att" + String(results.length)).textContent = t.toFixed(2);
         document.getElementById("target").textContent = getTarget();
         // document.getElementById("bpa").textContent = getBPA();
         // document.getElementById("wpa").textContent = getWPA();
-        document.getElementById("average").textContent = getAverage();
-        if (results.length == 5) {
-            maxR = sorted_result[4];
+        document.getElementById("average").textContent = getAverage(mode);
+
+        // add ( ) to results (max and min)
+        if (results.length == n && mode == "ao5") {
+            maxR = sorted_result[n - 1];
             minR = sorted_result[0];
+            let flagMax = false;
+            let flagMin = false;
             for (let i = 1; i <= 5; i++) {
-                if (Number(document.getElementById("att" + String(i)).textContent) == maxR ||
-                    Number(document.getElementById("att" + String(i)).textContent) == minR) {
+                if (!flagMax && document.getElementById("att" + String(i)).textContent == maxR) {
                     document.getElementById("att" + String(i)).textContent =
                         `(${document.getElementById("att" + String(i)).textContent})`;
+                    flagMax = true;
+                } else if (!flagMin && document.getElementById("att" + String(i)).textContent == minR) {
+                    document.getElementById("att" + String(i)).textContent =
+                        `(${document.getElementById("att" + String(i)).textContent})`;
+                    flagMin = true;
                 }
             }
         }
     } else {
         results = [];
         document.getElementById("average").textContent = "";
-        for (i = 1; i <= 5; i++) {
+        for (i = 1; i <= n; i++) {
             document.getElementById("att" + String(i)).textContent = "";
         }
         results.push(t);
@@ -169,6 +206,9 @@ function getBPA() {
     if (results.length < 4 || results.length == 5) {
         return ""
     } else if (results.length == 4) {
+        if (sorted_result[3] == "DNF" && sorted_result[2] == "DNF") {
+            return "DNF";
+        }
         return ((sorted_result[0] + sorted_result[1] + sorted_result[2]) / 3).toFixed(2);
     }
 }
@@ -177,17 +217,23 @@ function getWPA() {
     if (results.length < 4 || results.length == 5) {
         return ""
     } else if (results.length == 4) {
+        if (sorted_result[3] == "DNF" && sorted_result[2] == "DNF") {
+            return "DNF";
+        }
         return ((sorted_result[3] + sorted_result[1] + sorted_result[2]) / 3).toFixed(2);
     }
 }
 
 function getTarget() {
+    if (mode == "mo3") {
+        return "";
+    }
     if (results.length < 4 || results.length == 5) {
         return ""
     } else {
         if (Number(getWPA()) <= goal) {
             return "Guaranteed!";
-        } else if (Number(getBPA()) > goal) {
+        } else if (getBPA() == "DNF" || Number(getBPA()) > goal) {
             return "Impossible!";
         } else {
             return (goal * 3 - sorted_result[1] - sorted_result[2]).toFixed(2);
@@ -200,26 +246,82 @@ function getAverage(mode = "ao5") {
         if (results.length < 5) {
             return "";
         }
-        let avg = ((sorted_result[3] + sorted_result[1] + sorted_result[2]) / 3).toFixed(2);
-        printResults(avg);
+        let count_dnf = 0;
+        let r = [];
+        for (let i of results) {
+            if (i === "DNF") {
+                count_dnf++;
+            } else {
+                r.push(i);
+            }
+        }
+        r.sort((a, b) => a - b);
+        let avg = 0;
+        if (count_dnf > 1) {
+            return "DNF";
+        } else if (count_dnf == 1) {
+            avg = ((sorted_result[3] + sorted_result[1] + sorted_result[2]) / 3).toFixed(2);
+        }
+        avg = ((sorted_result[3] + sorted_result[1] + sorted_result[2]) / 3).toFixed(2);
+        // printResults(avg);
         return avg;
     } else if (mode == "mo3") {
         if (results.length < 3) {
             return "";
         }
+        for (let i of results) {
+            if (i === "DNF") {
+                return "DNF";
+            }
+        }
         return ((results[0] + results[1] + results[2]) / 3).toFixed(2);
     }
+}
+
+function mysort(r) {
+    let count_dnf = 0;
+    let list = [];
+    for (let i of r) {
+        if (i == "DNF") {
+            count_dnf++;
+        } else {
+            list.push(i);
+        }
+    }
+    list.sort((a, b) => a - b);
+    for (let a = 0; a < count_dnf; a++) {
+        list.push("DNF");
+    }
+    return list;
 }
 
 function reset() {
     timer_block.textContent = "0.00";
     results = [];
     for (i = 1; i <= 5; i++) {
-        document.getElementById("att" + String(i)).textContent = "";
+        clearTextContent(document.getElementById("att" + String(i)));
     }
-    document.getElementById("bpa").textContent = "";
-    document.getElementById("wpa").textContent = "";
-    document.getElementById("average").textContent = "";
+    clearTextContent(document.getElementById("bpa"));
+    clearTextContent(document.getElementById("wpa"));
+    clearTextContent(document.getElementById("average"));
+}
+
+function switchDNF() {
+    const index = results.length - 1;
+    if (timer_block.textContent != "DNF") {
+        timer_block.textContent = "DNF";
+        results[index] = "DNF";
+        document.getElementById("att" + String(index + 1)).textContent = "DNF";
+    } else {
+        timer_block.textContent = current_result.toFixed(2);
+        document.getElementById("att" + String(index + 1)).textContent = current_result.toFixed(2);
+        results[index] = current_result;
+    }
+}
+
+function clearTextContent(e) {
+    if (e)
+        e.textContent = "";
 }
 
 function resetTheme() {
@@ -239,6 +341,28 @@ function changeTheme(t) {
     resetTheme();
     changeStorage("theme", t);
     // console.log(`Theme changed to ${t}`);
+}
+
+function changeMode(m) {
+    reset();
+    mode = m;
+    if (m == "mo3") {
+        document.getElementById("col4").style.display = "none";
+        document.getElementById("col5").style.display = "none";
+        document.getElementById("target_title").style.display = "none";
+        document.getElementById("target").style.display = "none";
+        document.getElementById("att4").style.display = "none";
+        document.getElementById("att5").style.display = "none";
+
+    } else if (m == "ao5") {
+        document.getElementById("col4").style.display = "table-cell";
+        document.getElementById("col5").style.display = "table-cell";
+        document.getElementById("target_title").style.display = "table-cell";
+        document.getElementById("target").style.display = "table-cell";
+        document.getElementById("att4").style.display = "table-cell";
+        document.getElementById("att5").style.display = "table-cell";
+    }
+    changeStorage("mode", m);
 }
 
 function changeStorage(setting, value) {
@@ -266,8 +390,14 @@ window.addEventListener("DOMContentLoaded", async () => {
     if (theme) {
         changeTheme(theme);
     }
+
     const g = getVariable("goal");
     if (g) {
         goal = Number(g);
+    }
+
+    const m = getVariable("mode");
+    if (m) {
+        changeMode(m);
     }
 });
